da855ae8a66ea1f055d76a7c96098383652e97e9
Bruno Leite <bruno.marinho@useall.com.br>
2023-12-11 09:23:59 -0300

    (feat): auth module - [missing tests]

---
+++ src/domain/accounts/application/use-cases/authenticate-user.ts
@@ -0,0 +1,55 @@
+import { Either, left, right } from "@/core/either";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { Injectable } from "@nestjs/common";
+import { UsersRepository } from "../repositories/users-repository";
+import { HashComparer } from "../crypto/hash-comparer";
+import { Encrypter } from "../crypto/encrypter";
+
+interface AuthenticateUserUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+type AuthenticateUserUseCaseResponse = Either<
+  InvalidCredentialsError,
+  {
+    accessToken: string;
+  }
+>;
+
+@Injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      return left(new InvalidCredentialsError());
+    }
+
+    const passwordValid = await this.hashComparer.compare(
+      password,
+      user.password
+    );
+
+    if (!passwordValid) {
+      return left(new InvalidCredentialsError());
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: user.id.toString,
+    });
+
+    return right({
+      accessToken,
+    });
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,12 @@ import { DatabaseModule } from "../database/database.module";
 import { CryptographyModule } from "../crypto/crypto.module";
 import { CreateAccountController } from "./controllers/accounts/create-account.controller";
 import { RegisterUserUseCase } from "@/domain/accounts/application/use-cases/register-user";
+import { AuthenticateController } from "./controllers/accounts/authenticate.controller";
+import { AuthenticateUserUseCase } from "@/domain/accounts/application/use-cases/authenticate-user";
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateAccountController],
-  providers: [RegisterUserUseCase]
-}) 
+  controllers: [CreateAccountController, AuthenticateController],
+  providers: [RegisterUserUseCase, AuthenticateUserUseCase],
+})
 export class HttpModule {}
